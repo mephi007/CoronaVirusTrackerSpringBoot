e5600a0c43beff8275b472bebef8b3c2f0f56b6b
mephi007 <roysumit949@gmail.com>
2020-10-16 12:32:28 +0530

    added schedule to collect data every second

--- coronavirus-tracker/src/main/java/io/javaSumit/coronavirustracker/service/CoronaVirusDataService.java
+++ coronavirus-tracker/src/main/java/io/javaSumit/coronavirustracker/service/CoronaVirusDataService.java
@@ -12,6 +12,7 @@ import javax.annotation.PostConstruct;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -20,6 +21,7 @@ public class CoronaVirusDataService {
 	private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 	
 	@PostConstruct
+	@Scheduled(cron = "* * * * * *")
 	public void fetchVirusData() throws IOException, InterruptedException {
 		HttpClient client = HttpClient.newHttpClient();
 		HttpRequest request = HttpRequest.newBuilder().uri(URI.create(VIRUS_DATA_URL)).build();
