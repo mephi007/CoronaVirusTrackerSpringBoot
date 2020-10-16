2e5d71bcd717edd7036bb3222fac74deede10ae7
mephi007 <roysumit949@gmail.com>
2020-10-16 12:51:40 +0530

    adding data from Http response to LocationStat model

--- coronavirus-tracker/src/main/java/io/javaSumit/coronavirustracker/service/CoronaVirusDataService.java
+++ coronavirus-tracker/src/main/java/io/javaSumit/coronavirustracker/service/CoronaVirusDataService.java
@@ -7,6 +7,8 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 
@@ -15,11 +17,14 @@ import org.apache.commons.csv.CSVRecord;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import io.javaSumit.coronavirustracker.models.LocationStats;
+
 @Service
 public class CoronaVirusDataService {
 
 	private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 	
+	private List<LocationStats> allStats = new ArrayList();
 	@PostConstruct
 	@Scheduled(cron = "* * 1 * * *")
 	public void fetchVirusData() throws IOException, InterruptedException {
@@ -27,17 +32,19 @@ public class CoronaVirusDataService {
 		HttpRequest request = HttpRequest.newBuilder().uri(URI.create(VIRUS_DATA_URL)).build();
 		
 		HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
-		
-		System.out.println(httpResponse.body());
+		List<LocationStats> newStats = new ArrayList();
 		//coverting string to reader
 		StringReader csvBodyReader = new StringReader(httpResponse.body());
 		
 		Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
 		for (CSVRecord record : records) {
-		    String state = record.get("Province/State");
-		    System.out.println(state);
-//		    String customerNo = record.get("CustomerNo");
-//		    String name = record.get("Name");
+			LocationStats locationStat = new LocationStats();
+		    locationStat.setState(record.get("Province/State"));
+		    locationStat.setCountry(record.get("Country/Region"));
+		    locationStat.setLatestTotalCases(Integer.parseInt(record.get(record.size()-1)));
+		    System.out.println(locationStat);
+		    newStats.add(locationStat);
 		}
+		this.allStats = newStats;
 	}
 }
