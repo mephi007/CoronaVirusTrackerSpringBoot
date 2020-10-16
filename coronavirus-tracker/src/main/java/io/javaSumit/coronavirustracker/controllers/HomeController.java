b02c8ea1fc9b715e8bd74e99c7f1da4fe30b8dbd
mephi007 <roysumit949@gmail.com>
2020-10-16 13:04:28 +0530

    created rest API to render html page

--- coronavirus-tracker/src/main/java/io/javaSumit/coronavirustracker/controllers/HomeController.java
+++ coronavirus-tracker/src/main/java/io/javaSumit/coronavirustracker/controllers/HomeController.java
@@ -1,8 +1,13 @@
 package io.javaSumit.coronavirustracker.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
+	
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
 }
